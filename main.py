21c50edfadd5a3f13888eb6ab1b0aae49b58a35b
chikanerick <115743340+chikanerick@users.noreply.github.com>
2024-09-17 00:11:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import random
 rpc_url = RPC_URL
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 
-if not web3.isConnected():
-    raise Exception("Не удалось подключиться к сети Arbitrum")
+if not web3.is_connected():
+    raise Exception("[-] connect to Arbitrum network")
 
 contract_address = '0x78072889Ee4D7Fe1A100C25296AABBEA32e92Bea'
 method_id = '0xfb9d09c8'  
 quantity = 5
-
 
 with open('private_keys.txt', 'r') as file:
     wallets = [line.strip() for line in file]
@@ -26,7 +25,7 @@ def send_mint_transaction(wallet_private_key):
         wallet_address = account.address
 
         
-        calldata = method_id + web3.toHex(web3.toBytes(quantity)).lstrip("0x").zfill(64)
+        calldata = method_id + web3.to_hex(web3.to_bytes(quantity)).lstrip("0x").zfill(64)
 
         
         nonce = web3.eth.get_transaction_count(wallet_address)
@@ -35,9 +34,9 @@ def send_mint_transaction(wallet_private_key):
         transaction = {
             'from': wallet_address,
             'to': contract_address,
-            'value': web3.toWei(0, 'ether'),  
+            'value': web3.to_wei(0, 'ether'),  
             'gas': 2000000,  
-            'gasPrice': web3.toWei(0.01, 'gwei'),  
+            'gasPrice': web3.to_wei(0.01, 'gwei'),  
             'nonce': nonce,
             'data': calldata,
             'chainId': 42161,  
@@ -52,7 +51,7 @@ def send_mint_transaction(wallet_private_key):
 
       
         tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        print(f"Отправлена транзакция. Хэш: {web3.toHex(tx_hash)}")
+        print(f"[+] Transaction confirmed. Txn: {web3.to_hex(tx_hash)}")
 
        
         with open(successful_wallets_file, 'a') as f:
@@ -62,7 +61,7 @@ def send_mint_transaction(wallet_private_key):
 
     except Exception as e:
        
-        print(f"Ошибка при отправке транзакции с кошелька {wallet_address}: {e}")
+        print(f"[-] {wallet_address}: {e}")
         return False 
 
 
@@ -70,7 +69,8 @@ for private_key in wallets:
     success = send_mint_transaction(private_key)
     
     if success:
-        delay = random.choice(DELAY)
-        print(f"Задержка {delay} секунд")   
+        delay = random.choice(DELAY) 
+        print(f"[*] Sleep {delay} sec")   
+       
         time.sleep(delay)  
         
--- settings.py
+++ settings.py
@@ -1,3 +1,2 @@
 DELAY = [12,45] # Задержка между кошельками в секундах
-GAS_PRICE_THRESHOLD = 9 # Лимит газа, иначе софт ждет
-RPC_URL = 'https://arbitrum.llamarpc.com'
+RPC_URL = 'https://arb1.arbitrum.io/rpc'
